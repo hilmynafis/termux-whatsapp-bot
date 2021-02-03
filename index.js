c8d9e827725c5998c734d224801a3e918d0c1a5b
hilmynafis <78489163+hilmynafis@users.noreply.github.com>
2021-02-03 21:24:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ CFonts.say('WHATSAPP BOT', {
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say('\'termux-whatsapp-bot\' By @NazwaS', {
+CFonts.say('\'Hlmy Bot\' By Hlmy Nfs', {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
